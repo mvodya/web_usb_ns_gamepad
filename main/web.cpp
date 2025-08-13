ab21b7330a1f1498773a62968f21126f0f4f1cf1
Mark Vodyanitskiy <mvodya@icloud.com>
2025-08-13 19:41:41 +1000

    Added http server

--- main/web.cpp
+++ main/web.cpp
@@ -1,11 +1,14 @@
 #include "web.hpp"
 
+#include "cJSON.h"
 #include "esp_err.h"
 #include "esp_event.h"
+#include "esp_http_server.h"
 #include "esp_log.h"
 #include "esp_wifi.h"
 #include "esp_wifi_types_generic.h"
 #include "freertos/idf_additions.h"
+#include "projdefs.h"
 
 // Convert option NSG_WIFI_SCAN_AUTH_MODE_THRESHOLD -> wifi_auth_mode_t
 #if CONFIG_NSG_WIFI_AUTH_OPEN
@@ -30,6 +33,10 @@ namespace WEB {
 
 static const char* TAG = "app web";
 
+struct web_server_ctx {
+  int todo;
+};
+
 // Event group to signal, when WiFi connected
 static EventGroupHandle_t wifi_event_group;
 #define WIFI_CONNECTED_BIT BIT0  // Connected to the AP with an IP
@@ -48,11 +55,13 @@ void wifi_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id
     // Connection failed
     if (wifi_connect_retry_num < CONFIG_NSG_WIFI_MAXIMUM_RETRY) {
       // Retry connection
+      vTaskDelay(pdMS_TO_TICKS(200));
       esp_wifi_connect();
       wifi_connect_retry_num++;
       ESP_LOGI(TAG, "Retry to connect to the AP (%i)", wifi_connect_retry_num);
     } else {
       // Faild to connect
+      ESP_LOGE(TAG, "Failed to connect to the AP SSID: %s", CONFIG_NSG_WIFI_SSID);
       xEventGroupSetBits(wifi_event_group, WIFI_FAIL_BIT);
     }
   } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
@@ -66,7 +75,7 @@ void wifi_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id
 }
 
 // Setup WiFi connection
-void wifi_init_sta_task(void*) {
+esp_err_t wifi_init_sta() {
   ESP_LOGI(TAG, "WiFi STA initialization");
 
   wifi_event_group = xEventGroupCreate();
@@ -86,16 +95,17 @@ void wifi_init_sta_task(void*) {
   ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP,
                                                       &wifi_event_handler, NULL, &instance_got_ip));
 
-  wifi_config_t wifi_config = {
-      .sta =
-          {
-              .ssid = CONFIG_NSG_WIFI_SSID,
-              .password = CONFIG_NSG_WIFI_PASSWORD,
-              .threshold = {.authmode = NSG_WIFI_SCAN_AUTH_MODE_THRESHOLD},
-          },
-  };
+  wifi_config_t wifi_config = {};
+  memset(&wifi_config, 0, sizeof(wifi_config_t));
+  strncpy((char*)wifi_config.sta.ssid, CONFIG_NSG_WIFI_SSID, sizeof(wifi_config.sta.ssid));
+  strncpy((char*)wifi_config.sta.password, CONFIG_NSG_WIFI_PASSWORD,
+          sizeof(wifi_config.sta.password));
+  wifi_config.sta.listen_interval = 10;
+  wifi_config.sta.threshold.rssi = -127;
+  wifi_config.sta.threshold.authmode = NSG_WIFI_SCAN_AUTH_MODE_THRESHOLD;
   ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
   ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+  ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_PS_NONE));
   ESP_ERROR_CHECK(esp_wifi_start());
 
   ESP_LOGI(TAG, "WiFi STA initialization DONE");
@@ -112,16 +122,62 @@ void wifi_init_sta_task(void*) {
     ESP_LOGE(TAG, "Unexpected error while WiFi AP connection (SSID: %s)", CONFIG_NSG_WIFI_SSID);
   }
 
-  ESP_LOGI(TAG, "wifi_init_sta_task is finished");
-  vTaskDelete(NULL);
+  return ESP_OK;
+}
+
+// API: Test ping API
+esp_err_t api_rest_ping(httpd_req_t* req) {
+  httpd_resp_set_type(req, "application/json");
+  cJSON* root = cJSON_CreateObject();
+
+  cJSON_AddStringToObject(root, "answer", "pong");
+  const char* data = cJSON_Print(root);
+  httpd_resp_sendstr(req, data);
+  free((void*)data);
+  cJSON_Delete(root);
+
+  return ESP_OK;
+}
+
+// Setup HTTP/RESTful server
+esp_err_t web_server_init() {
+  ESP_LOGI(TAG, "WEB server initialization");
+
+  web_server_ctx server_ctx = {};
+
+  // Setup HTTP server
+  httpd_handle_t server = NULL;
+  httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+  config.uri_match_fn = httpd_uri_match_wildcard;
+
+  ESP_LOGI(TAG, "Starting HTTP Server");
+  ESP_ERROR_CHECK(httpd_start(&server, &config));
+
+  /* URI handler for fetching temperature data */
+  httpd_uri_t temperature_data_get_uri = {
+      .uri = "/api/ping", .method = HTTP_GET, .handler = api_rest_ping, .user_ctx = &server_ctx};
+  httpd_register_uri_handler(server, &temperature_data_get_uri);
+
+  return ESP_OK;
+}
+
+// Task for init & manage web server
+void web_task(void*) {
+  ESP_LOGI(TAG, "WEB task runned");
+
+  wifi_init_sta();
+  web_server_init();
+
+  while (1) {
+    vTaskDelay(pdMS_TO_TICKS(100));
+  }
 }
 
 // Setup web component
 esp_err_t init() {
   ESP_LOGI(TAG, "WEB component initialization");
 
-  ESP_LOGI(TAG, "Start WiFi init task");
-  xTaskCreate(wifi_init_sta_task, "wifi_init_sta_task", 4096, NULL, 2, NULL);
+  xTaskCreate(web_task, "web_task", 4096, NULL, 3, NULL);
 
   return ESP_OK;
 }
